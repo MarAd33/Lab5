e5e8a260cee73b8a7f975de2a3699ab54a685962
MarAd33 <36238078+MarAd33@users.noreply.github.com>
2018-03-21 04:26:46 -0700

    task 5,6,7

--- lab5.R
+++ lab5.R
@@ -1,10 +1,64 @@
-barplot(table(mtcars$cyl))
-plot(x = mtcars$disp, y = mtcars$cyl, type = "s")
-plot(x = mtcars$wt, y = mtcars$mpg)
-hist(mtcars$mpg)
-plot(mtcars$cyl, mtcars$mpg)
-plot(as.factor(mtcars$cyl), mtcars$mpg)
-hist(mtcars$mpg)
-barplot(table(mtcars$cyl))
-barplot(table(mtcars$cyl),col='blue',main='cylinders')
-barplot(table(mtcars$gear),col='blue',main='gears')
+AP=AirPassengers
+AP1 = unclass(AP)
+years=c('0')
+
+i=0
+for (item in 1948:1960)
+{
+  years[i]=item
+  i=i+1
+}
+
+
+A=matrix(AP1,nrow=12,ncol=12,byrow=TRUE)
+class(A)
+
+Months_data=apply(A, 2, sum)
+Years_data=apply(A, 1, sum)
+print ("The most profitable month of the 12 years")
+month.abb[which.max(Months_data)]
+print("with the revenue of")
+print(max(Months_data))
+print ("The most profitable year of the 12 years")
+years[which.max(Years_data)]
+print("with the revenue of")
+print(max(Years_data))
+print("company's growth over the 12 year period")
+plot(AP.matrix,xlab="Date", ylab = "Passenger numbers (1000's)",main="Air Passenger numbers from 1949 to 1961")
+
+
+price=c(8000)
+for(i in 2:12)
+{
+  price[i]=price[i-1]+price[i-1]*(10/100)
+}
+
+out <- matrix(NA, nrow=12, ncol=12)
+for(i in 1:12)
+{
+  
+   out[i,]<-c(A[i,]*price[i])
+}
+out
+Months_data=apply(out, 2, sum)
+Years_data=apply(out, 1, sum)
+print ("The highest revenue month of the 12 years")
+month.abb[which.max(Months_data)]
+print("with the revenue of")
+print(max(Months_data))
+print ("The highest revenue year of the 12 years")
+years[which.max(Years_data)]
+print("with the revenue of")
+print(max(Years_data))
+print("Total revenue of the 12 years")
+print(sum(Years_data))
+months_of_year=list(c(0,0))
+for( i in 1:12)
+{
+  vec=which(A[i,] == max(A[i,]))
+  print("The most travelled months of")
+  print(years[i])
+  print("are")
+  print(month.abb[vec])
+}
+
